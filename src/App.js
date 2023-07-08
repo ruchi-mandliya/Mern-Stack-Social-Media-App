e8a9b29afb6c898532c96a330b1fad1b415af5e7
Ruchi Mandliya <rmandliya18@gmail.com>
2023-07-08 21:56:15 +0530

    Add: blur background

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+function App() {
+  return (
+    <div className="App">
+      <div className="blur1" style={{ top: "-10px", right: "-5rem" }}></div>
+      <div className="blur2" style={{ top: "45px", left: "-5rem" }}></div>
+    </div>
+  );
+}
+
+export default App;
